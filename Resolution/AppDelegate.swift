629a1750616b226241aecdd72569b8246de534a0
Zachary Drayer <zacharydrayer@gmail.com>
2015-08-09 00:58:58 -0700

    Initial Commit

---
+++ Resolution/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Resolution
+//
+//  Created by Zachary on 8/9/15.
+//  Copyright © 2015 z. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
